a18b177ce15e456ab1bd6b31689f7bc22bf0e774
masonproj <83470841+masonproj@users.noreply.github.com>
2023-07-15 17:45:20 -0700

    bug warning

---
+++ src/pages/BugAlert.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const BugAlert = () => {
+  const [dismissed, setDismissed] = useState(false);
+
+  const handleDismiss = () => {
+    setDismissed(true);
+  };
+
+  if (dismissed) {
+    return null; // Hide the alert if it has been dismissed
+  }
+
+  return (
+    <div className="bg-red-100 text-red-800 p-4 mb-4 rounded">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <svg
+            className="w-5 h-5 mr-2"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12zm1-8a1 1 0 11-2 0 1 1 0 012 0zm-1 4a1 1 0 100-2 1 1 0 000 2z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span className="font-bold">Important:</span> This site is currently in a buggy or experimental state.
+        </div>
+        <button
+          className="text-red-600 hover:text-red-800 font-semibold focus:outline-none"
+          onClick={handleDismiss}
+        >
+          Dismiss
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BugAlert;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import CSVParser from '../pages/CSVParser';
+import BugAlert from './BugAlert';
 
 const HomePage = () => {
   return (
     <div>
+      <BugAlert />
       <CSVParser />
     </div>
   );
