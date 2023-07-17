5a03c292b3a81f71e7ea7437cc9dbb4d37d69c97
masonproj <83470841+masonproj@users.noreply.github.com>
2023-07-16 22:37:56 -0700

    componentified table

--- src/pages/PlayerFilters.js
+++ src/pages/PlayerFilters.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Table from './Table';
+import Table from '../components/Table';
 
 const PlayerFilters = ({ csvData, setCSVData }) => {
   const [filteredData, setFilteredData] = useState([]);
